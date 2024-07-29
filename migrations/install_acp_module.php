da33930a0deeee15a6014b381bce8b3e9f9c2a05
sebolp <fabiomassimo.loprete@gmail.com>
2024-07-29 14:33:49 +0200

    Add files via upload

---
+++ migrations/install_acp_module.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * PostReaction. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2024, sebo, https://www.fiatpandaclub.org
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace sebo\postreact\migrations;
+
+class install_acp_module extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['sebo_postreact_goodbye']);
+	}
+
+	public static function depends_on()
+	{
+		return ['\phpbb\db\migration\data\v320\v320'];
+	}
+
+	public function update_data()
+	{
+		return [
+			//['config.add', ['sebo_postreact_goodbye', 0]],
+
+			['module.add', [
+				'acp',
+				'ACP_CAT_DOT_MODS',
+				'ACP_POSTREACT_TITLE'
+			]],
+			['module.add', [
+				'acp',
+				'ACP_POSTREACT_TITLE',
+				[
+					'module_basename'	=> '\sebo\postreact\acp\main_module',
+					'modes'				=> ['settings'],
+				],
+			]],
+		];
+	}
+}
