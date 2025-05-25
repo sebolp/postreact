9cf63ca31d719fff243464a4bb418a3305ff132c
sebolp <fabiomassimo.loprete@gmail.com>
2025-05-25 13:04:51 +0200

    1.3.1

--- ext/sebo/postreact/styles/prosilver/theme/bubble.js
+++ ext/sebo/postreact/styles/prosilver/theme/bubble.js
@@ -1,23 +1,26 @@
-/* open */
 document.addEventListener("DOMContentLoaded", function () {
-	document.querySelectorAll(".toggle-react-summary").forEach(button => {
-		button.addEventListener("click", function (e) {
-			e.preventDefault();
-			let postId = this.getAttribute("data-post-id");
-			document.getElementById("darken").style.display = "block";
-			document.getElementById("phpbb_confirm_" + postId).style.display = "block";
-		});
-	});
-});
+    document.querySelectorAll('.react-toggle').forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('data-target');
+            const popup = document.getElementById(targetId);
 
-/* close */
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".alert_close").forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault(); // Evita il comportamento predefinito del link
-            let postId = this.getAttribute("data-post-id");
-			document.getElementById("darken").style.display = "none";
-			document.getElementById("phpbb_confirm_" + postId).style.display = "none";
-		});
-	});
+            // close other pupus
+            document.querySelectorAll('.popup_pr').forEach(p => {
+                if (p !== popup) p.style.display = 'none';
+            });
+
+            // Toggle
+            popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+        });
+    });
+
+    // Close out
+    document.addEventListener('click', function (e) {
+        const isToggle = e.target.closest('.react-toggle');
+        const isPopup = e.target.closest('.popup_pr');
+        if (!isToggle && !isPopup) {
+            document.querySelectorAll('.popup_pr').forEach(p => p.style.display = 'none');
+        }
+    });
 });
